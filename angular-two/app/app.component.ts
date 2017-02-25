a3b98cc5be46cfeb9358cf7c4f1ab3c73d270221
jperez <jperez>
2017-02-25 22:37:06 +0100

    Añadir modelo de datos, ng-class, ng-click

--- angular-two/app/app.component.ts
+++ angular-two/app/app.component.ts
@@ -1,21 +1,33 @@
 // Importar el n�cleo de Angular
 import {Component} from 'angular2/core';
+import {Pelicula} from './model/pelicula';
  
 // Decorador component, indicamos en que etiqueta se va a cargar la plantilla
 @Component({
     selector: 'my-app',
-	templateUrl: 'app/view/peliculas.html'
+	templateUrl: 'app/view/peliculas.html',
+	styleUrls: ["app/assets/css/styles.css"]
 })
  
 // Clase del componente donde iran los datos y funcionalidades
 export class AppComponent { 
-	public titulo:string;
-	public pelicula:string;
-	public anio:number;
+	public titulo:string = "Mis peliculas Angular";
+	public pelicula:Pelicula;
+	public mostrarDatos:boolean;
+
+	
 
 	constructor(){
-		this.titulo = "Peliculas con Angular 2"; 
-		this.pelicula="Amores perros";
-		this.anio =1987;
+		this.pelicula = new Pelicula(1, "Amores Perros", "Juan de la Fuente", 1987);
+		this.mostrarDatos = false;
+		this.debug();
+	}
+
+	debug(){
+		console.log(this.pelicula);
+	}
+
+	onShowHide(){
+		this.mostrarDatos= !this.mostrarDatos;
 	}
 }
